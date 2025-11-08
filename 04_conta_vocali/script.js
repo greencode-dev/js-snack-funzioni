41500fa6929809aebae1b7f3dd7001d23c009262
greencode-dev <antonioverde.dev@gmail.com>
2025-11-08 11:06:25 +0100

    quarto esercizio completato

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -2,12 +2,20 @@
 
 const word = 'javascript';
 
-
 // Dichiara la funzione qui.
-
+function contaVocali(string) {
+  // [aeiou] -> cerca i caratteri a, e, i, o, u
+  // g -> ricerca globale (non si ferma alla prima occorrenza)
+  // i -> case-insensitive (non distingue tra maiuscole e minuscole)
+  return string.match(/[aeiou]/gi).length;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+const vocaliContate = contaVocali(word);
+console.log(vocaliContate);
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
+
+//Stampa il risultato in pagina
+const resultElement = document.getElementById('result');
+resultElement.innerText = `Nella parola "${word}" ci sono ${vocaliContate} vocali.`;
