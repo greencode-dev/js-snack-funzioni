a0409938a8fb78965c0dcef25058af2436e3429d
greencode-dev <antonioverde.dev@gmail.com>
2025-11-08 10:07:38 +0100

    secondo esercizio completato

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -1,13 +1,18 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
-const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
-
+const names = ['Anna', 'Luca', 'Marco', 'Adele', 'Giovanni', 'Alessandra'];
 
 // Dichiara la funzione qui.
-
+function iniziali(array) {
+  return array.map((name) => name.charAt(0));
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(iniziali(names));
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+
+// Stampa in Pagina:
+const output = document.getElementById('result');
+output.innerHTML = iniziali(names);
